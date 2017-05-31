60e2bd9d16208cf2afcb98c0b4487ebb0f80a318
Rezo Imnadze <rezo@revenuedevelopment.org>
2017-05-31 12:32:53 +0400

    Executing recognision

--- webapp/views.py
+++ webapp/views.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse , HttpResponseRedirect
 from django.shortcuts import render
+import os
+import subprocess
 
 
-from .forms import UploadFileForm
-
-# Imaginary function to handle an uploaded file.
-from .forms import handle_uploaded_file
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'upload.html', {'form': form})
-
-# Create your views here.
+work_dir = os.path.dirname(os.path.realpath(__file__))
 
 def index(request):
-	return render(request, 'webapp/home.html')
+  if request.method == 'POST':
+    image_path = os.path.join(work_dir, 'uploads', request.FILES['source'].name)
+    with open(image_path, 'wb+') as destination:
+        for chunk in request.FILES['source'].chunks():
+            destination.write(chunk)
 
+    
+    ocr_executable = os.path.join(work_dir, '../../georgian-ocr-v2/bin/ocr.sh')
+    export_executable = os.path.join(work_dir, '../../georgian-ocr-v2/bin/export_words.sh')
 
+    subprocess.check_output([ocr_executable, image_path])
+    recognized_text = subprocess.check_output([export_executable, image_path])
+      
 
+    return render(request, 'webapp/home.html', { 
+      'recognized_text': recognized_text
+      })
+  else:
+    return render(request, 'webapp/home.html')
