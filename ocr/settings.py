b40f645125b16f7bf1d03bb4159c4d23ab00843c
Nutura <ninouturashvili@gmail.com>
2017-06-12 20:14:38 +0400

    added styles

--- ocr/settings.py
+++ ocr/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# print BASE_DIR + ' ' + 'aqane vpostant chven'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -118,4 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+print "static url" , STATIC_URL
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "/static"),
+#    '/var/www/static/',
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "ocr/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "ocr/static")
+print "BASE_DIR" , "./ocr"
+print "static root" , STATIC_ROOT
